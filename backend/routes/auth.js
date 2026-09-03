cc301eee39a337f940e0c55f8c0d5b47e9921b95
Vivek Singh Bhandari <viveksinghbhandari12@gmail.com>
2026-09-04 01:45:58 +0530

    second  commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,8 +17,8 @@ function signToken(user) {
 function setTokenCookie(res, token) {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 }
